76d097598a71a6c7f05566e533b5a56c5affec4d
Unknown <jme919@gmail.com>
2018-04-07 21:43:27 -0400

    work on spotify function

--- liri.js
+++ liri.js
@@ -1,8 +1,8 @@
 require("dotenv").config();
 var request = require("request");
 var myKeys = require("./keys.js");
-var twitter = require("twitter");
-var spotify = require("node-spotify-api");
+var Twitter = require("twitter");
+var Spotify = require("node-spotify-api");
 var fs = require("fs");
 
 
@@ -15,7 +15,8 @@ var spotify = new Spotify(myKeys.spotify);
 var client = new Twitter(myKeys.twitter);
 
 //variable for users input//
-var userRequest = process.argv[2];
+var userCommand = process.argv[2];
+var userRequest = process.argv[3];
 
 
 
@@ -23,38 +24,78 @@ var userRequest = process.argv[2];
 
 
 function mytweets(){
-  request("https://api.twitter.com/1.1/statuses/user_timeline.json?Tunammez=" + client + "&count=20", function(error, response, body) {
+  var param = {
+  	screen_name: "tunammez",
+  	count: 20
+  };
+  client.get("statuses/user_timeline", param, function(error, tweets, response){
 
   // If the request was successful...
   if (!error && response.statusCode === 200) {
+  	for (var j = 0; j < tweets.length; j++){
+  		var theTweets = ("Tweet " + (j + 1) + ":" + tweets[j].created_at + " " + tweets[j].text);
+  		console.log(theTweets);
+  	}
 
-    // Then log the body from the site!
-    console.log(body);
+    
   }
+  if(error) throw error;
+
 });
 
 
-};
+}
 
-if(userRequest === "my-tweets"){
+if(userCommand === "my-tweets"){
 	mytweets();
 
 }
 
+console.log("it reached here");
+
 
 //show artist, song name, preview link, and album from spotify//
 function spotifyThisSong(){
+	var defaultSong = "the sign";
 
-};
+if(userCommand === "spotifyThisSong"){
+	spotifyThisSong();
+	 }
+
+if(userRequest ===undefinded){
+	userRequest = defaultSong;
+}
+
+	spotify.search({type: "track", query: userRequest, limit: 1}).then(function(response, err){
+		console.log(response);
+		for(var i = 0; i < data.tracks.items.length; i++){
+		if (!err){
+			console.log("Song: " + response.tracks.items[i].name);
+			console.log("Artist: " + response.track.items[i].artists[0].name);
+			console.log("Album: " + response.tracks.items[i].album.name);
+			console.log("Preview Link: " + response.tracks.items[i].preview_url);
+		  }
+		  
+		}
+	}).catch(function(err) {
+            console.log(err);
+		
+       });
+	
+	
+	
+}
+
+
 
 
 //Show movie title, year, IMDB rating, Rotten Tom rating, country, language, plot, actors//
 function movieThis(){
 
-};
+}
 
 
 //use fs Node package, LIRI will take the text inside random.txt and the use it to call one of the LIRI commands
 function doWhatItSays(){
 
-};
+}
